9a6c084487be40486ca11da3c517968f7811739e
hayden <hayden.ismet@amigotechnology.com>
2022-11-17 22:57:16 +0000

    adding more data

--- backend/controllers/all-animals-controller.js
+++ backend/controllers/all-animals-controller.js
@@ -2,11 +2,33 @@ const fetch = require("node-fetch");
 
 const getAllAnimals = async (req, res, next) => {
   // in requesting access to users, this network request is literally just setting the response json to be called users with the value of the DUMMY_USERS
-  const response = await fetch(
+  const catsProtectionResponse = await fetch(
     "https://www.cats.org.uk/umbraco/surface/catsearch/searchcats?longitude=-2.01580047607422&latitude=52.4455108642578&withCats=false&withDogs=false&withChildren=false&withFamilies=false&indoorsOnly=false"
   );
-  const data = await response.json();
-  res.json({ cats_protection: await data });
+  const catsProtectionData = await catsProtectionResponse.json();
+
+  const blueCrossCatResponse = await fetch(
+    "https://www.bluecross.org.uk/pet/listing/cat"
+  );
+  const blueCrossCatData = await blueCrossCatResponse.json();
+
+  const blueCrossDogResponse = await fetch(
+    "https://www.bluecross.org.uk/pet/listing/dog"
+  );
+  const blueCrossDogData = await blueCrossDogResponse.json();
+
+  const batterseaDogsResponse = await fetch(
+    "https://www.battersea.org.uk/api/animals/dogs"
+  );
+
+  const batterseaDogsData = await batterseaDogsResponse.json();
+
+  res.json({
+    cats_protection: await catsProtectionData,
+    blue_cross_cat: await blueCrossCatData.results,
+    blue_cross_dog: await blueCrossDogData.results,
+    battersea_dogs: await batterseaDogsData,
+  });
 };
 
 // to export multiples dont use module.exports, use (but dont execute with (), just passing reference) :

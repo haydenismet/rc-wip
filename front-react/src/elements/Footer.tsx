fdabd24bf5898a45a1d02730d8ef8be9608ab7e9
hayden <hayden.ismet@amigotechnology.com>
2022-11-28 17:15:26 +0000

    footer static css

--- front-react/src/elements/Footer.tsx
+++ front-react/src/elements/Footer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import logo from "../assets/rc-logo.png";
+import sneakyKitty from "../assets/sneaky-kitty.png";
 export default function Footer() {
     return (<>
         <div className="footer">
-            Footer
+
+            <img src={sneakyKitty} alt="cat" className="sneaky-kitty" />
+            <div className="footer-copy">Pooling adoption centres online to help you find your purrfect friend. </div>
+            <div className="footer-logo"><img src={logo} alt="rescue collective logo" /></div>
+
         </div>
     </>)
 }

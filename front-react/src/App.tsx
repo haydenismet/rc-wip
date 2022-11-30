cc0a7d79aaa5226bafa7d06dcbacc902de9d6b0b
hayden <hayden.ismet@amigotechnology.com>
2022-11-30 17:25:57 +0000

    favourites basic setup, style tweaks

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import Home from "./page/Home";
 import AnimalDetail from "./page/AnimalDetail";
+import Favourites from "./page/Favourites";
 import "./styles/App.css";
-/*<Home></Home>*/
+/*<Home/>
+<AnimalDetail/>
+*/
 function App() {
   return (
-    <AnimalDetail></AnimalDetail >
+
+    <Favourites />
   );
 }
 
--- front-react/src/page/Favourites.tsx
+++ front-react/src/page/Favourites.tsx
@@ -2,31 +2,20 @@ import React from "react";
 import Hero from "../elements/Hero";
 import Navbar from "../elements/Navbar";
 import TopHeader from "../elements/TopHeader";
-import Filter from "../elements/Filter";
+
 import Listing from "../elements/Listing";
 import Footer from "../elements/Footer";
-import LoadMore from "../elements/LoadMore";
-import Popup from "../elements/Popup";
+
 
 // function types
 // prop types
-export default function Home() {
+export default function Favourites() {
     return (<>
         <TopHeader />
         <div className="container">
             <Navbar />
             <Hero />
-            <Filter />
-            <div className="listing">
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
+            <div className="favourites-container">
                 <Listing />
                 <Listing />
                 <Listing />
@@ -34,7 +23,6 @@ export default function Home() {
                 <Listing />
                 <Listing />
             </div>
-            <LoadMore />
         </div>
         <Footer />
         {/* <Popup /> */}
